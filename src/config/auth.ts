8e6f202b51bee5ff2437608ac5dfddfbaa9bedff
PabloMelo <pablomelodev@gmail.com>
2021-04-20 17:52:57 -0300

    feat: create middleware authenttication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'newsecreteman',
+    expiresIn: '7d',
+  },
+};
---
+++ src/middlewares/authenticationAssurance.ts
@@ -0,0 +1,30 @@
+import { verify } from 'jsonwebtoken';
+import { AuthChecker } from 'type-graphql';
+
+import auth from '../config/auth';
+
+interface Context {
+  token?: string;
+}
+
+const authenticationAssurance: AuthChecker<Context> = ({
+  context,
+}): boolean => {
+  const authHeader = context.token;
+
+  if (!authHeader) {
+    return false;
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, auth.jwt.secret);
+
+    return !!decoded;
+  } catch {
+    return false;
+  }
+};
+
+export { authenticationAssurance };
